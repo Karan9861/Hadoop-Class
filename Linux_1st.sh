70ad2832993356adf2eb35f10167f9c806d11d1f
Karan9861 <112437822+Karan9861@users.noreply.github.com>
2023-02-04 08:48:52 +0530

    Add files via upload

---
+++ Linux_1st.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo hello world
+
+pwd
+
+ls
+
+cd del
+
+cat file10
+cd	
+
+mkdir linuxbasics01	
+
+cd linuxbasics01	
+
+
+touch file01	
+ll				
+nano file01			
+cat file01			
+mv file01 file1			
+mv file1 testfolder/file1		
+mv testfolder/file1 file1		
+
+cp file1 testfolder/file2
+cp -r testfolder test1
+
+which python3		
+
+cu root
+
+sudo apt install 
